358efb634a631081622b74b11432240eab8b6619
minjae <alswo2722@uos.ac.kr>
2018-07-06 15:54:21 +0900

    그림자 옵션, 카메라 메소드, UI구성중

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
 <template>
   <div id="app">
-    <div id="cesiumContainer">
-        <el-menu class="camera" mode="horizontal" @select="handleMenuBar" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
-            <el-submenu index="1">
-                <template slot="title">DMZ</template>
-                <el-menu-item index="1-1">middle view</el-menu-item>
-                <el-menu-item index="1-2">origin model</el-menu-item>
-                <el-menu-item index="1-3">texture model</el-menu-item>
-            </el-submenu>
-            <el-menu-item index="2">세운상가</el-menu-item>
-            <el-menu-item index="3">뉴욕</el-menu-item>
-        </el-menu>
+    <div id="cesiumContainer"></div>
+    <div id="toolbar">
+        <div id="location">
+            <p>Location</p>
+            <el-radio-group v-model="location_model" size="small" v-on:change="this.locationCamera">
+            <el-radio-button label="DMZ_Peace_Plaza"></el-radio-button>
+            <el-radio-button label="SewoonCampus"></el-radio-button>
+            <el-radio-button label="NewYork"></el-radio-button>
+            </el-radio-group>
+        </div>
+        <div id="option">
+            <p>Option</p>
+            <span>shadow</span>
+            <el-switch v-model="shadow_model" v-on:change="this.showShadow" ></el-switch>
+        </div>
+        <div>
+            <p>ArtWork</p>
+            <!-- <span>None Texture</span>
+            <el-select v-model="none_texture_model" multiple collapse-tags style="margin-left: 20px;" placeholder="Select">
+                <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
+            </el-select> -->
+            <el-table :data="artworks_model" style="width: 100%">
+                <el-table-column prop="model" label="Model" width="100"></el-table-column>
+                <el-table-column prop="texture" label="Texture" width="100"></el-table-column>
+                <el-table-column prop="color" label="Color" width="100"></el-table-column>
+                <el-table-column prop="opercity" label="Opercity" width="100"></el-table-column>
+            </el-table>
+            <!-- <br/>
+            <span>Texture</span>
+            <el-select v-model="texture_model" multiple collapse-tags style="margin-left: 20px;" placeholder="Select">
+                <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
+            </el-select> -->
+        </div>
     </div>
   </div>
 </template>
@@ -19,58 +41,80 @@
 import Cesium from 'cesium/Cesium';
 import widgets from 'cesium/Widgets/widgets.css';
 
-import CameraModel from './components/cameraModel'
-
 export default {
     name: 'app',
-    components: {
-        'CameraModel' : CameraModel,
-    },
     data() {
         return {
             viewer: '',
-            url: window.location.href.slice(0,-3),
-            scene: ''
+            url: window.location.href,
+            location_model: "",
+            location_options: {
+                DMZ_Peace_Plaza : [127.230294, 38.307178, 250.0],
+                SewoonCampus : [126.995151, 37.569879, 1000.0],
+                NewYork : [ -73.981061, 40.719265, 100000.0]
+            },
+            shadow_model: true,
+            artworks_model : [{
+                model : '1-s1'
+            },{
+                model : '1-s2'
+            },{
+                model : '2-s7'
+            },{
+                model : '3-s3'
+            },{
+                model : '3-s5'
+            },{
+                model : '3-s6'
+            },{
+                model : '4-s4'
+            },],
+            none_texture_model: [],
+            texture_model: [],
         }
     },
     mounted() {
-        this.loadCesium()
+        this.initCesium()
+        this.createModel(this.url + '/static/none/1-s1.glb',[127.2310400972056,38.30786173349542],0)
+        this.createModel(this.url + '/static/none/1-s2.glb',[127.2309034247928,38.30776363257965],0)
+        this.createModel(this.url + '/static/none/2-s7.glb',[127.2306922000855,38.30771498710235],0)
+        this.createModel(this.url + '/static/none/3-s3.glb',[127.2306505028071,38.30759306518442],0)
+        this.createModel(this.url + '/static/none/3-s5.glb',[127.2307207813936,38.30754055902372],0)
+        this.createModel(this.url + '/static/none/3-s6.glb',[127.2308125550497,38.30751561222556],0)
+        this.createModel(this.url + '/static/none/4-s4.glb',[127.230836112512,38.3075715439865],0)
+
+        // this.createModel(this.url + '/static/1-s2.glb',[127.2309034247928,38.30776363257965],0)
+        // this.createModel(this.url + '/static/2-s7.glb',[127.2306922000855,38.30771498710235],0)
+        // this.createModel(this.url + '/static/3-s3.glb',[127.2306505028071,38.30759306518442],0)
+        // this.createModel(this.url + '/static/3-s5.glb',[127.2307207813936,38.30754055902372],0)
+        // this.createModel(this.url + '/static/3-s6.glb',[127.2308125550497,38.30751561222556],0)
+        // this.createModel(this.url + '/static/4-s4.glb',[127.230836112512,38.3075715439865],0)
     },
+
+    computed: {
+
+    },
+
     methods: {
-        loadCesium() {
+        // 세시움 마운트 메소드
+        initCesium() {
             this.viewer = new Cesium.Viewer('cesiumContainer', {
-                animation: false,
-                baseLayerPicker: false,
-                fullscreenButton: false,
+                shadows: true,
                 geocoder: false,
-                homeButton: false,
-                infoBox: false,
-                sceneModePicker: false,
-                selectionIndicator: true,
-                timeline: false,
-                navigationHelpButton: false,
-                scene3DOnly: true,
                 imageryProvider: new Cesium.MapboxImageryProvider({
                     mapId: 'mapbox.satellite',
                     accessToken: 'pk.eyJ1Ijoic2V3b29uIiwiYSI6ImNqaTEyZnBmZTA2NTEzcXFwNTRvNjFmdDMifQ.vHg1EEUA3N_yY8-LyHlgUw'
-                })
+                }),
             });
             var options = {
                 camera : this.viewer.scene.camera,
                 canvas : this.viewer.scene.canvas
             };
             this.viewer.dataSources.add(Cesium.KmlDataSource.load(this.url + '/static/dmz.kml', options));
-            // this.viewer.dataSources.add(Cesium.KmlDataSource.load('/static/dmz.kml', options));
         },
-
-        flytoCamera(location) {
-            this.viewer.camera.flyTo({
-                destination : Cesium.Cartesian3.fromDegrees(location[0], location[1], location[2]),
-            });
-        },
-
+        // ArtWork 모델 생성 메소드
         createModel(url, location, height) {
-            // this.viewer.entities.removeAll();
+            this.viewer.entities.removeAll();
 
             var position = Cesium.Cartesian3.fromDegrees(location[0], location[1], height);
             var heading = 0;
@@ -85,52 +129,37 @@ export default {
                 orientation : orientation,
                 model : {
                     uri : url,
-                    scale: 1,
+                    scale: 0.5,
                     minimumPixelSize : 10,
-                    maximumScale : 100
+                    maximumScale : 100,
+                    color : new Cesium.Color(60, 120, 120, 0.5),
                 }
             });
-            // viewer.trackedEntity = entity;
         },
-        handleMenuBar(key, keyPath) {
-            console.log(key);
-            var middleView = [127.230294, 38.307178, 100000.0]
-            var dmz = [127.230294, 38.307178, 250.0]
-            var sewoon = [126.995151, 37.569879, 1000.0]
-            var newyork = [-73.981061, 40.719265, 100000.0]
-            switch(String(key)) {
-                case '1-1':
-                    this.flytoCamera(middleView)
+        // 그림자 옵션 메소드
+        showShadow: function () {
+            this.viewer.shadows = this.shadow_model
+        },
+        // 카메라 이동 메소드
+        flytoCamera(location) {
+            this.viewer.camera.flyTo({
+                destination : Cesium.Cartesian3.fromDegrees(location[0], location[1], location[2]),
+            });
+        },
+        // 지역 이동 카메라 메소드
+        locationCamera: function (location) {
+            switch(location) {
+                case "DMZ_Peace_Plaza":
+                    this.flytoCamera(this.location_options.DMZ_Peace_Plaza);
                     break;
-                case '1-2':
-                    this.flytoCamera(dmz)
-                    this.createModel(this.url + '/static/1-s1.glb',[127.2310400972056,38.30786173349542],0)
-                    this.createModel(this.url + '/static/1-s2.glb',[127.2309034247928,38.30776363257965],0)
-                    this.createModel(this.url + '/static/2-s7.glb',[127.2306922000855,38.30771498710235],0)
-                    this.createModel(this.url + '/static/3-s3.glb',[127.2306505028071,38.30759306518442],0)
-                    this.createModel(this.url + '/static/3-s5.glb',[127.2307207813936,38.30754055902372],0)
-                    this.createModel(this.url + '/static/3-s6.glb',[127.2308125550497,38.30751561222556],0)
-                    this.createModel(this.url + '/static/4-s4.glb',[127.230836112512,38.3075715439865],0)
-                    // this.createModel('/static/1-s1.glb',[127.2310400972056,38.30786173349542],0)
-                    // this.createModel('/static/1-s2.glb',[127.2309034247928,38.30776363257965],0)
-                    // this.createModel('/static/2-s7.glb',[127.2306922000855,38.30771498710235],0)
-                    // this.createModel('/static/3-s3.glb',[127.2306505028071,38.30759306518442],0)
-                    // this.createModel('/static/3-s5.glb',[127.2307207813936,38.30754055902372],0)
-                    // this.createModel('/static/3-s6.glb',[127.2308125550497,38.30751561222556],0)
-                    // this.createModel('/static/4-s4.glb',[127.230836112512,38.3075715439865],0)
+                case "SewoonCampus":
+                    this.flytoCamera(this.location_options.SewoonCampus);
                     break;
-                case '1-3':
-                    this.flytoCamera(dmz)
-                    break;
-                case '2':
-                    this.flytoCamera(sewoon)
-                    break;
-                case '3':
-                    this.flytoCamera(newyork)
+                case "NewYork":
+                    this.flytoCamera(this.location_options.NewYork);
                     break;
             }
-        }
-
+        },
     },
 
 }
@@ -144,11 +173,35 @@ html, body, #cesiumContainer {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
+    color: #ffffff;
     width: 100%;
     height: 100%;
 }
+#cesiumContainer {
+    position: absolute;
+}
+#toolbar {
+    background: rgba(42, 42, 42, 0.8);
+    padding: 4px;
+    border-radius: 4px;
+    z-index: 1000;
+    position: absolute;
+}
+#toolbar input {
+    vertical-align: middle;
+    padding-top: 2px;
+    padding-bottom: 2px;
+}
+#toolbar .header {
+    font-weight: bold;
+}
+#toolbar p {
+    margin-bottom: 8px;
+    margin-top: 8px;
+}
+
 .el-menu--horizontal {
     border-bottom: 0px;
 }
+
 </style>
