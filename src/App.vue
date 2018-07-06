1466c6fa97ae9ce48f8486b64dc34c7a67f146b9
minjae <alswo2722@uos.ac.kr>
2018-07-06 20:23:24 +0900

    전시 작품 완성

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@
     <div id="toolbar">
         <div id="location">
             <p>Location</p>
-            <el-radio-group v-model="location_model" size="small" v-on:change="this.locationCamera">
-            <el-radio-button label="DMZ_Peace_Plaza"></el-radio-button>
-            <el-radio-button label="SewoonCampus"></el-radio-button>
-            <el-radio-button label="NewYork"></el-radio-button>
+            <el-radio-group v-model="location_model" size="small" @change="this.locationCamera">
+                <el-radio-button label="DMZ_Peace_Plaza"></el-radio-button>
+                <el-radio-button label="SewoonCampus"></el-radio-button>
+                <el-radio-button label="NewYork"></el-radio-button>
             </el-radio-group>
         </div>
         <div id="option">
@@ -17,21 +17,32 @@
         </div>
         <div>
             <p>ArtWork</p>
-            <!-- <span>None Texture</span>
-            <el-select v-model="none_texture_model" multiple collapse-tags style="margin-left: 20px;" placeholder="Select">
-                <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
-            </el-select> -->
-            <el-table :data="artworks_model" style="width: 100%">
-                <el-table-column prop="model" label="Model" width="100"></el-table-column>
-                <el-table-column prop="texture" label="Texture" width="100"></el-table-column>
-                <el-table-column prop="color" label="Color" width="100"></el-table-column>
-                <el-table-column prop="opercity" label="Opercity" width="100"></el-table-column>
+            <el-table ref="multipleTable" :data="artworks_model" @selection-change="handleSelectionChange" style="width: 100%">
+                    <el-table-column type="selection" width="55"></el-table-column>
+                <el-table-column label="Model" width="100">
+                    <template slot-scope="scope">
+                        <i class="el-icon-location-outline"></i>
+                        <span style="margin-left: 10px">{{ scope.row.model }}</span>
+                    </template>
+                </el-table-column>
+                <el-table-column label="Texture" width="100">
+                    <template slot-scope="scope">
+                        <el-switch v-model="scope.row.texture_model" active-color="#13ce66" inactive-color="#ff4949" ></el-switch>
+                    </template>
+                </el-table-column>
+                <el-table-column label="Color" width="100">
+                    <template slot-scope="scope">
+                          <el-select v-model="scope.row.color_model" placeholder="Select">
+                            <el-option v-for="item in color_option" :key="item.value" :label="item.label" :value="item.value"></el-option>
+                        </el-select>
+                    </template>
+                </el-table-column>
+                <el-table-column label="Opercity" width="100">
+                    <template slot-scope="scope">
+                        <el-slider v-model="scope.row.opercity_model" :format-tooltip="formatTooltip"></el-slider>
+                    </template>
+                </el-table-column>
             </el-table>
-            <!-- <br/>
-            <span>Texture</span>
-            <el-select v-model="texture_model" multiple collapse-tags style="margin-left: 20px;" placeholder="Select">
-                <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
-            </el-select> -->
         </div>
     </div>
   </div>
@@ -47,6 +58,8 @@ export default {
         return {
             viewer: '',
             url: window.location.href,
+            none_texture_url: window.location.href + 'static/none/' ,
+            texture_url: window.location.href + 'static/texture/',
             location_model: "",
             location_options: {
                 DMZ_Peace_Plaza : [127.230294, 38.307178, 250.0],
@@ -54,45 +67,118 @@ export default {
                 NewYork : [ -73.981061, 40.719265, 100000.0]
             },
             shadow_model: true,
+            multipleSelection: [],
             artworks_model : [{
-                model : '1-s1'
+                id: 0,
+                model : '1-s1',
+                texture_model: false,
+                color_model: 'GREY',
+                // url: [window.location.href + 'static/none/1.glb',window.location.href + 'static/texture/1.glb'],
+                opercity_model : 100
             },{
-                model : '1-s2'
+                id:1,
+                model : '1-s2',
+                texture_model: false,
+                color_model: 'GREY',
+                opercity_model : 50
             },{
-                model : '2-s7'
+                id:2,
+                model : '2-s7',
+                texture_model: false,
+                color_model: 'GREY',
+                opercity_model : 100
             },{
-                model : '3-s3'
+                id: 3,
+                model : '3-s3',
+                texture_model: false,
+                color_model: 'GREY',
+                opercity_model : 100
             },{
-                model : '3-s5'
+                id: 4,
+                model : '3-s5',
+                texture_model: false,
+                color_model: 'GREY',
+                opercity_model : 100
             },{
-                model : '3-s6'
+                id: 5,
+                model : '3-s6',
+                texture_model: false,
+                color_model: 'GREY',
+                opercity_model : 100
             },{
-                model : '4-s4'
-            },],
-            none_texture_model: [],
-            texture_model: [],
+                id: 6,
+                model : '4-s4',
+                texture_model: false,
+                color_model: 'GREY',
+                opercity_model : 100
+            }],
+            artworks_option: [{
+                name: 0,
+                url: '',
+                location: [127.2310400972056, 38.30786173349542],
+                color: 'GREY',
+                opercity: 1.0
+            },{
+                name: 1,
+                url: '',
+                location: [127.2309034247928, 38.30776363257965],
+                color: 'GREY',
+                opercity: 1.0
+            },{
+                name: 2,
+                url: '',
+                location: [127.2306922000855,38.30771498710235],
+                color: 'GREY',
+                opercity: 1.0
+            },{
+                name: 3,
+                url: '',
+                location: [127.2306505028071,38.30759306518442],
+                color: 'GREY',
+                opercity: 1.0
+            },{
+                name: 4,
+                url: '',
+                location: [127.2307207813936,38.30754055902372],
+                color: 'GREY',
+                opercity: 1.0
+            },{
+                name: 5,
+                url: '',
+                location: [127.2308125550497,38.30751561222556],
+                color: 'GREY',
+                opercity: 1.0
+            },{
+                name: 6,
+                url: '',
+                location: [127.230836112512,38.3075715439865],
+                color: 'GREY',
+                opercity: 1.0
+            }],
+
+            color_option: [{
+                value: 'Red',
+                label: 'Red'
+            }, {
+                value: 'Blue',
+                label: 'Blue'
+            }, {
+                value: 'Green',
+                label: 'Green'
+            }, {
+                value: 'White',
+                label: 'White'
+            }, {
+                value: 'Black',
+                label: 'Black'
+            },{
+                value: 'GREY',
+                label: 'GREY'
+            }],
         }
     },
     mounted() {
         this.initCesium()
-        this.createModel(this.url + '/static/none/1-s1.glb',[127.2310400972056,38.30786173349542],0)
-        this.createModel(this.url + '/static/none/1-s2.glb',[127.2309034247928,38.30776363257965],0)
-        this.createModel(this.url + '/static/none/2-s7.glb',[127.2306922000855,38.30771498710235],0)
-        this.createModel(this.url + '/static/none/3-s3.glb',[127.2306505028071,38.30759306518442],0)
-        this.createModel(this.url + '/static/none/3-s5.glb',[127.2307207813936,38.30754055902372],0)
-        this.createModel(this.url + '/static/none/3-s6.glb',[127.2308125550497,38.30751561222556],0)
-        this.createModel(this.url + '/static/none/4-s4.glb',[127.230836112512,38.3075715439865],0)
-
-        // this.createModel(this.url + '/static/1-s2.glb',[127.2309034247928,38.30776363257965],0)
-        // this.createModel(this.url + '/static/2-s7.glb',[127.2306922000855,38.30771498710235],0)
-        // this.createModel(this.url + '/static/3-s3.glb',[127.2306505028071,38.30759306518442],0)
-        // this.createModel(this.url + '/static/3-s5.glb',[127.2307207813936,38.30754055902372],0)
-        // this.createModel(this.url + '/static/3-s6.glb',[127.2308125550497,38.30751561222556],0)
-        // this.createModel(this.url + '/static/4-s4.glb',[127.230836112512,38.3075715439865],0)
-    },
-
-    computed: {
-
     },
 
     methods: {
@@ -112,11 +198,23 @@ export default {
             };
             this.viewer.dataSources.add(Cesium.KmlDataSource.load(this.url + '/static/dmz.kml', options));
         },
+        // 작품 색상 및 투명도
+         getColor(colorName, alpha) {
+            var color = Cesium.Color[colorName.toUpperCase()];
+            return Cesium.Color.fromAlpha(color, parseFloat(alpha));
+        },
+        // 작품 투명도 비율
+        formatTooltip(opercity) {
+            return opercity / 100;
+        },
+        isTexture(checkedTexture, url) {
+            return checkedTexture ? url[1] : url[0]
+        },
         // ArtWork 모델 생성 메소드
-        createModel(url, location, height) {
-            this.viewer.entities.removeAll();
+        createModel(name, url, location, color, opercity) {
+            // this.viewer.entities.removeAll();
 
-            var position = Cesium.Cartesian3.fromDegrees(location[0], location[1], height);
+            var position = Cesium.Cartesian3.fromDegrees(location[0], location[1]);
             var heading = 0;
             var pitch = 0;
             var roll = 0;
@@ -124,7 +222,7 @@ export default {
             var orientation = Cesium.Transforms.headingPitchRollQuaternion(position, hpr);
 
             this.viewer.entities.add({
-                name : url,
+                name : name,
                 position : position,
                 orientation : orientation,
                 model : {
@@ -132,7 +230,7 @@ export default {
                     scale: 0.5,
                     minimumPixelSize : 10,
                     maximumScale : 100,
-                    color : new Cesium.Color(60, 120, 120, 0.5),
+                    color : this.getColor(color, opercity)
                 }
             });
         },
@@ -160,6 +258,32 @@ export default {
                     break;
             }
         },
+        // 다중 선택
+        handleSelectionChange: function (data) {
+            this.multipleSelection = data;
+            this.viewer.entities.removeAll();
+            data.forEach((result)=> {
+                if(this.artworks_model[result.id].texture_model == true) {
+                    this.artworks_option[result.id].url = this.texture_url + String(result.id+1) + '.glb'
+                } else {
+                    this.artworks_option[result.id].url = this.none_texture_url + String(result.id+1) + '.glb'
+                }
+                console.log(this.artworks_option[result.id].url)
+                // this.artworks_option[result.id].url = this.artworks_model[result.id].texture_model
+                this.artworks_option[result.id].color = this.artworks_model[result.id].color_model
+                this.artworks_option[result.id].opercity = this.artworks_model[result.id].opercity_model
+                console.log(this.artworks_option[result.id])
+                this.createModel(
+                    this.artworks_option[result.id].name,
+                    this.artworks_option[result.id].url,
+                    this.artworks_option[result.id].location,
+                    this.artworks_option[result.id].color,
+                    this.artworks_option[result.id].opercity/100
+                )
+
+            })
+            // this.createModel(this)
+        }
     },
 
 }
